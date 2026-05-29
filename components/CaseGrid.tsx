6ea9c0f45f42fb4da81209e3ab68986fe2238681
Naim3097 <sales@nexovadigital.com>
2026-05-29 15:26:12 +0800

    Show expand-icon (white circle) on every feature project card image

--- components/CaseGrid.tsx
+++ components/CaseGrid.tsx
@@ -185,16 +185,14 @@ export default function CaseGrid({
             <div className="case__media">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img src={c.img} alt={c.alt} loading="lazy" />
-              {c.zoom && (
-                <span className="case__zoom" aria-label={filterLabels.viewProjectLabel}>
-                  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M15 3h6v6" />
-                    <path d="M9 21H3v-6" />
-                    <path d="M21 3l-7 7" />
-                    <path d="M3 21l7-7" />
-                  </svg>
-                </span>
-              )}
+              <span className="case__zoom" aria-label={filterLabels.viewProjectLabel}>
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M15 3h6v6" />
+                  <path d="M9 21H3v-6" />
+                  <path d="M21 3l-7 7" />
+                  <path d="M3 21l7-7" />
+                </svg>
+              </span>
             </div>
             <div className="case__body">
               <div className="case__meta">
